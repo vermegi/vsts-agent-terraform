b062923ff710b2355b38dad31049d8ea0e012c7d
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2018-11-28 08:28:02 +0000

    added fnuction code

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,52 @@
-FROM microsoft/vsts-agent:latest
+FROM microsoft/vsts-agent:ubuntu-14.04
 
-# Build-time metadata as defined at http://label-schema.org
-LABEL org.label-schema.name="VSTS Agent with Terraform Tools" \
-    org.label-schema.url="https://github.com/vermegi/" \
-    org.label-schema.vcs-url="https://github.com/vermegi/vsts-agent-terraform" \
-    org.label-schema.schema-version="1.0"
-                
-ENV TERRAFORM_VERSION 0.11.8
+# To make it easier for build and release pipelines to run apt-get,
+# configure apt to not require confirmation (assume the -y argument by default)
+ENV DEBIAN_FRONTEND=noninteractive
+RUN echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
-# Install Terraform
-RUN echo "===> Installing Terraform ${TERRAFORM_VERSION}..." \
- && wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
- &&	unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
- && mv terraform /usr/local/bin/terraform \
- && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip 
+# Install basic command-line utilities
+RUN apt-get update \
+ && apt-get install -y --no-install-recommends \
+    curl \
+    dnsutils \
+    file \
+    ftp \
+    iproute2 \
+    iputils-ping \
+    locales \
+    openssh-client \
+    rsync\
+    shellcheck \
+    sudo \
+    telnet \
+    time \
+    unzip \
+    wget \
+    zip \
+    tzdata \
+ && rm -rf /var/lib/apt/lists/*
+
+ # Setup the locale
+ENV LANG en_US.UTF-8
+ENV LC_ALL $LANG
+RUN locale-gen $LANG \
+ && update-locale
+
+# Accept EULA - needed for certain Microsoft packages like SQL Server Client Tools
+ENV ACCEPT_EULA=Y
+
+# Install essential build tools
+RUN apt-get update \
+ && apt-get install -y --no-install-recommends \
+    build-essential \
+ && rm -rf /var/lib/apt/lists/*
  
+ # Install Terraform
+RUN TERRAFORM_VERSION=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r .current_version) \
+ && curl -LO https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+ && unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+ && rm -f terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+
+#docker run -e VSTS_ACCOUNT=givermei -e VSTS_TOKEN=7cs5p7onl34xzchvrzwywpjxb6qh3dypj34bzyupofp5lhosm45a -it vsts-agent-terraform
---
+++ VSTSBuildAgent/VSTSBuildAgent/VSTSBuildAgent.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace VSTSBuildAgent
+{
+    public static class VSTSBuildAgent
+    {
+        private static IAzure _azure = null;
+
+        [FunctionName("StartVSTSBuildAgent")]
+        public static async Task<HttpResponseMessage> StartVSTSBuildAgentAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            var rg = await AzureConn.ResourceGroups.GetByNameAsync(ConfigurationManager.AppSettings["resourceGroupName"]);
+            var agentName = await GetNameAsync(req, "agentName");
+            var env = new Dictionary<string, string>
+            {
+                { "VSTS_AGENT_INPUT_URL", ConfigurationManager.AppSettings["VSTS_AGENT_INPUT_URL"] },
+                { "VSTS_ACCOUNT", ConfigurationManager.AppSettings["VSTS_ACCOUNT"] },
+                { "VSTS_AGENT_INPUT_AUTH", "pat" },
+                { "VSTS_AGENT_INPUT_TOKEN", ConfigurationManager.AppSettings["VSTS_AGENT_INPUT_TOKEN"] },
+                { "VSTS_TOKEN", ConfigurationManager.AppSettings["VSTS_AGENT_INPUT_TOKEN"] },
+                { "VSTS_AGENT_INPUT_POOL", "ACIPool" },
+                { "VSTS_POOL", "ACIPool" },
+                { "VSTS_AGENT_INPUT_AGENT", agentName },
+                { "VSTS_AGENT", agentName }
+            };
+
+            var containerGroup = await AzureConn.ContainerGroups.Define(agentName)
+                .WithRegion(rg.RegionName)
+                .WithExistingResourceGroup(rg)
+                .WithLinux()
+                .WithPublicImageRegistryOnly()
+                .WithoutVolume()
+                .DefineContainerInstance(agentName)
+                    .WithImage(ConfigurationManager.AppSettings["imageName"])
+                    .WithoutPorts()
+                    .WithEnvironmentVariables(env)
+                    .Attach()
+                .CreateAsync();
+
+            return req.CreateResponse(HttpStatusCode.OK, "VSTS agent is running");
+        }
+
+        [FunctionName("StopVSTSBuildAgent")]
+        public static async Task<HttpResponseMessage> StopVSTSBuildAgenttAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            var agentName = await GetNameAsync(req, "agentName");
+            await _azure.ContainerGroups.DeleteByResourceGroupAsync(ConfigurationManager.AppSettings["resourceGroupName"], agentName);
+            return req.CreateResponse(HttpStatusCode.OK, "VSTS agent has been removed");
+        }
+
+        private static async Task<string> GetNameAsync(HttpRequestMessage req, string key)
+        {
+            // parse query parameter
+            var name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Equals(q.Key, key, StringComparison.OrdinalIgnoreCase))
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            return (name ?? (string)data?.name).ToLower();
+        }
+        private static IAzure AzureConn
+        {
+            get
+            {
+                if (_azure == null)
+                { 
+                    var tenantId = ConfigurationManager.AppSettings["tenantId"];
+                    var sp = new ServicePrincipalLoginInformation
+                    {
+                        ClientId = ConfigurationManager.AppSettings["clientId"],
+                        ClientSecret = ConfigurationManager.AppSettings["clientSecret"]
+                    };
+                    _azure = Azure.Authenticate(new AzureCredentials(sp, tenantId, AzureEnvironment.AzureGlobalCloud)).WithDefaultSubscription();
+                }
+                return _azure;
+            }
+        }
+    }
+}
